2fcf8358c5c0c18a1d387ef1879acaa3e2a81206
Mayank <mayankverma76342@gmail.com>
2021-04-11 10:53:12 +0530

    Updates

---
+++ precache-manifest.77cf9094023ff312943213223309eadb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc7c5aaaab6b848803326c9bbdaf22bb",
+    "url": "/redis-demo/index.html"
+  },
+  {
+    "revision": "c95b52d8e98656dd4cda",
+    "url": "/redis-demo/static/css/main.780623d3.chunk.css"
+  },
+  {
+    "revision": "37b2204cdf046660f0ba",
+    "url": "/redis-demo/static/js/2.6609102b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/redis-demo/static/js/2.6609102b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c95b52d8e98656dd4cda",
+    "url": "/redis-demo/static/js/main.d65439bc.chunk.js"
+  },
+  {
+    "revision": "3eb2712e4e02b3a9817b",
+    "url": "/redis-demo/static/js/runtime-main.2ae57137.js"
+  }
+]);
